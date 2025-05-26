97462be14c881b89b2b24ea8bb3c1e83601327ec
Michal Grega <misogrega21@gmail.com>
2025-05-26 12:27:10 +0200

    B feature

--- util/feature_B.py
+++ util/feature_B.py
@@ -194,7 +194,6 @@ if __name__ == "__main__":
         visualize=False  # Set to True to see processing steps
     )
     
-    
     # Calculate border scores
     df_with_scores = calculate_border_score(df)
     
